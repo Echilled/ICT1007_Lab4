87089175e63f74ed3112dbf5deb37863da2b3137
Xenomii <voltzxp@gmail.com>
2021-03-12 13:56:47 +0800

    Added comments for code explanation.

--- producer_consumer.c
+++ producer_consumer.c
@@ -22,6 +22,7 @@ int getRandomNum(int);
 
 int main(int argc, char * argv[])
 {
+    /* Seed the random number generator */
     srand(time(NULL));
     int sleepNo, pNo, cNo;
 
@@ -33,36 +34,50 @@ int main(int argc, char * argv[])
     cNo = atoi(argv[3]);
     printf("\nNumber of consumers = %d", cNo);
 
+    /* Initialize the semaphores
+     * empty = 5
+     * full = 0 */
     sem_init(&empty, 0, 5);
     sem_init(&full, 0, 0);
 
+    /* Create pthreads according to number specified in the argument
+     * Store the identifier in an array in order to pass it as a parameter to the necessary functions */
     pthread_t pro[pNo], con[cNo];
     int proCount[pNo], conCount[cNo];
 
     for (int i = 0; i < pNo; ++i) {
+        /* Create an identifier for the thread and store it into the array */
         proCount[i] = i+1;
+        /* Create the thread and pass the identifier as a parameter to the producer function */
         pthread_create(&pro[i], NULL, &producer, &proCount[i]);
         printf("\nProducer-%d thread created successfully", i+1);
     }
 
     for (int i = 0; i < cNo; ++i) {
+        /* Create an identifier for the thread and store it into the array */
         conCount[i] = i+1;
+        /* Create the thread and pass the identifier as a parameter to the consumer function */
         pthread_create(&con[i], NULL, &consumer, &conCount[i]);
         printf("\nConsumer-%d thread created successfully", i+1);
     }
 
+    /* Main thread will sleep based on number specified in the argument */
     sleep(sleepNo);
     printf("\nSleep time elapsed. Producers and Consumers terminated.");
+    /* All the producers and consumers should not be running now */
     active = false;
 
     for (int i = 0; i < pNo; ++i) {
+        /* Wait for the threads to terminate */
         pthread_join(pro[i], NULL);
     }
 
     for (int i = 0; i < cNo; ++i) {
+        /* Wait for the threads to terminate */
         pthread_join(con[i], NULL);
     }
 
+    /* Count the amount of leftover items in the array */
     int leftover = 0;
     for (int i = 0; i < 5; ++i) {
         if (buffer[i] != 0)
@@ -70,6 +85,7 @@ int main(int argc, char * argv[])
     }
     printf("\nNumber of items in buffer = %d\n", leftover);
 
+    /* Destroy the mutex & semaphores */
     pthread_mutex_destroy(&test_mutex);
     sem_destroy(&empty);
     sem_destroy(&full);
@@ -77,21 +93,30 @@ int main(int argc, char * argv[])
     exit(0);
 }
 
+/* Function for consumer threads */
 void *consumer(void *arg) {
+    /* Convert the parameter passed in into an integer (identifier) */
     int thread_number = *((int *) arg);
 
     do {
+        /* Variable to hold the number to be consumed from buffer */
         int nextNom;
 
+        /* sem_wait will decrement full
+         * if full = 0, it will block until it gets incremented */
         if (active)
             sem_wait(&full);
         else
             return NULL;
 
+        /* Mutex lock */
         pthread_mutex_lock(&test_mutex);
 
+        /* Get the value of full and put into full_value
+         * This is to tell the thread which element in the buffer is the most recent "full" value */
         int full_value;
         sem_getvalue(&full, &full_value);
+        printf("\nCurrent value in semaphore %d", full_value);
 
         nextNom = buffer[full_value];
         buffer[full_value] = 0;
@@ -99,47 +124,63 @@ void *consumer(void *arg) {
         if (active)
             printf("\nConsumer-%d removed item \t%d", thread_number, nextNom);
 
+        /* Mutex unlock */
         pthread_mutex_unlock(&test_mutex);
 
+        /* Increment empty to signal that there are more free slots in the buffer */
         if (active)
             sem_post(&empty);
 
+        /* Sleep based on random number between 1 to 3 */
         sleep(getRandomNum(3));
 
     } while (active);
+    /* ??? */
     sem_post(&full);
     return NULL;
 }
 
 void *producer(void *arg) {
+    /* Convert the parameter passed in into an integer (identifier) */
     int thread_number = *((int *) arg);
 
     do {
+        /* Generate a random number */
         int nextItem = getRandomNum(MY_RAND_MAX);
 
+        /* Get value of empty
+         * If empty = 0, it will block until it gets incremented */
         if (active)
             sem_wait(&empty);
         else
             return NULL;
 
+        /* Mutex lock */
         pthread_mutex_lock(&test_mutex);
 
+        /* Get the value of full and put into full_value
+         * This is to tell the thread which element in the buffer is the most recent "full" value */
         int full_value;
         sem_getvalue(&full, &full_value);
+        printf("\nCurrent value in semaphore %d", full_value);
 
         buffer[full_value] = nextItem;
 
         if (active)
             printf("\nProducer-%d inserted item %d", thread_number, nextItem);
 
+        /* Mutex unlock */
         pthread_mutex_unlock(&test_mutex);
 
+        /* Increment full to signal that there are less free slots in the buffer */
         if (active)
             sem_post(&full);
 
+        /* Sleep based on random number between 1 to 3 */
         sleep(getRandomNum(3));
 
     } while (active);
+    /* ??? */
     sem_post(&empty);
     return NULL;
 }
